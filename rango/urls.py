18785b12823ecc3a911d4b699782e42b89434238
Du Phan <ptthanhdu@gmail.com>
2016-03-06 16:31:05 +0100

    add static and media

--- rango/urls.py
+++ rango/urls.py
@@ -3,5 +3,5 @@ from rango import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'), # matche to an empty string
-	url(r'about$', views.about, name='about')	
+	url(r'^about', views.about, name='about')	
 ]
